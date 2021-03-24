f9f9d4dd03bb5a7f9cb003821fa657c5414ec376
Rudi Giesler <rudi@praekelt.org>
2021-03-24 15:16:01 +0200

    Add validation for FlowResponse values according to question type

--- flows/models.py
+++ flows/models.py
@@ -214,3 +214,160 @@ class FlowResponse(models.Model):
     @response.setter
     def response(self, value):
         self.response_type, self.response_value = self._serialize(value)
+
+    def _validate_choice_order(self, errors):
+        choices = self.question.type_options["choices"]
+        choice_order = self.response_metadata["choice_order"]
+        if not isinstance(choice_order, list):
+            errors["response_metadata"].append("choice_order must be an array")
+        elif not all(choice in choice_order for choice in choices):
+            errors["response_metadata"].append(
+                f"choice_order {choice_order} contains choices not in {choices}"
+            )
+
+    def _validate_media(self, errors):
+        if not isinstance(self.response, URL) and isinstance(self.response, str):
+            self.response = URL(str)
+        if not isinstance(self.response, URL):
+            errors["response"].append("must be a URL")
+        if "format" in self.response_metadata:
+            if not isinstance(self.response_metadata["format"], str):
+                errors["response_metadata"].append("format must be a string")
+        if "dimensions" in self.response_metadata:
+            dimensions = self.response_metadata["dimensions"]
+            if not isinstance(dimensions, list):
+                errors["response_metadata"].append("dimensions must be an array")
+            else:
+                if len(dimensions) != 2:
+                    errors["response_metadata"].append(
+                        "dimensions must have a length of 2"
+                    )
+                if not all(isinstance(item, int) for item in dimensions):
+                    errors["response_metadata"].append(
+                        "dimensions items must be integers"
+                    )
+        if "file_size_mb" in self.response_metadata:
+            file_size_mb = self.response_metadata["file_size_mb"]
+            if not isinstance(file_size_mb, int) and not isinstance(
+                file_size_mb, float
+            ):
+                errors["response_metadata"].append(
+                    "file_size_mb must be integer or float"
+                )
+        if "duration_s" in self.response_metadata:
+            duration_s = self.response_metadata["duration_s"]
+            if not isinstance(duration_s, int) and not isinstance(duration_s, float):
+                errors["response_metadata"].append(
+                    "duration_s must be integer or float"
+                )
+        if "language" in self.response_metadata:
+            language = self.response_metadata["language"]
+            if not isinstance(language, str):
+                errors["response_metadata"].append("language must be a string")
+
+    def clean(self):
+        errors = defaultdict(list)
+        type_ = self.question.type
+        type_options = self.question.type_options
+
+        if type_ == FlowQuestion.Type.OPEN:
+            type_options = {}
+            valid_types = {
+                t.value for t in FlowQuestion.Type if t is not FlowQuestion.Type.OPEN
+            }
+            if "type" not in self.response_metadata:
+                errors["response_metadata"].append("type is required")
+            elif self.response_metadata["type"] not in valid_types:
+                errors["response_metadata"].append(
+                    f"type must be one of {sorted(valid_types)}"
+                )
+            else:
+                type_ = self.response_metadata["type"]
+
+            if "type_options" not in self.response_metadata:
+                errors["response_metadata"].append("type_options is required")
+            elif not isinstance(self.response_metadata["type_options"], dict):
+                errors["response_metadata"].append("type_options must be an object")
+            else:
+                type_options = self.response_metadata["type_options"]
+
+            FlowQuestion.validate_type_options(errors, type_, type_options)
+            if errors:
+                errors["response_metadata"].extend(
+                    [f"type_options.{e}" for e in errors.pop("type_options", [])]
+                )
+                raise ValidationError(errors)
+
+        if type_ == FlowQuestion.Type.SELECT_ONE:
+            choices = type_options["choices"]
+            if self.response not in choices:
+                errors["response"].append(
+                    f"{self.response} is not a valid choice. Valid choices are "
+                    f"{choices}"
+                )
+            if "choice_order" in self.response_metadata:
+                self._validate_choice_order(errors)
+        elif type_ == FlowQuestion.Type.SELECT_MANY:
+            choices = type_options["choices"]
+            if not isinstance(self.response, list):
+                errors["response"].append("must be an array")
+            elif not all(r in choices for r in self.response):
+                errors["response"].append(
+                    f"{self.response} contains choices not in {choices}"
+                )
+            if "choice_order" in self.response_metadata:
+                self._validate_choice_order(errors)
+        elif type_ == FlowQuestion.Type.NUMERIC:
+            if not isinstance(self.response, int) and not isinstance(
+                self.response, float
+            ):
+                errors["response"].append("must be float or integer")
+        elif type_ == FlowQuestion.Type.TEXT:
+            if not isinstance(self.response, str):
+                errors["response"].append("must be a string")
+            if "language" in self.response_metadata:
+                language = self.response_metadata["language"]
+                if not isinstance(language, str):
+                    errors["response_metadata"].append("language must be a string")
+        elif type_ == FlowQuestion.Type.IMAGE:
+            self._validate_media(errors)
+        elif type_ == FlowQuestion.Type.VIDEO:
+            self._validate_media(errors)
+        elif type_ == FlowQuestion.Type.AUDIO:
+            self._validate_media(errors)
+        elif type_ == FlowQuestion.Type.GEO_POINT:
+            if not isinstance(self.response, list):
+                errors["response"].append("must be an array")
+            else:
+                if not all(isinstance(item, float) for item in self.response):
+                    errors["response"].append("array may only contain floats")
+                if len(self.response) < 2 or len(self.response) > 4:
+                    errors["response"].append(
+                        "number of array elements must be between 2 and 4 inclusive"
+                    )
+        elif type_ == FlowQuestion.Type.DATETIME:
+            if isinstance(self.response, str):
+                try:
+                    self.response = datetime.fromisoformat(self.response)
+                except ValueError:
+                    pass
+            if not isinstance(self.response, datetime):
+                errors["response"].append("must be an RFC 3339 date-time")
+        elif type_ == FlowQuestion.Type.DATE:
+            if isinstance(self.response, str):
+                try:
+                    self.response = date.fromisoformat(self.response)
+                except ValueError:
+                    pass
+            if not isinstance(self.response, date):
+                errors["response"].append("must be an RFC 3339 date")
+        elif type_ == FlowQuestion.Type.TIME:
+            if isinstance(self.response, str):
+                try:
+                    self.response = time.fromisoformat(self.response)
+                except ValueError:
+                    pass
+            if not isinstance(self.response, time):
+                errors["response"].append("must be an RFC 3339 time")
+        if errors:
+            raise ValidationError(errors)
--- flows/test_models.py
+++ flows/test_models.py
@@ -122,6 +122,16 @@ class FlowQuestionTests(TestCase):
 
 
 class FlowResponseTests(TestCase):
+    def create_response(self):
+        return FlowResponse(
+            question=FlowQuestion.objects.create(flow=Flow.objects.create()),
+            session_id=1,
+            row_id=1,
+            contact_id=1,
+            response="",
+            response_metadata={},
+        )
+
     def test_row_id(self):
         """
         row_id should be either an int or a string
@@ -208,7 +218,7 @@ class FlowResponseTests(TestCase):
 
     def test_response(self):
         """
-        response can be any of the types, and should serialise and deserialise proeprly
+        response can be any of the types, and should serialise and deserialise properly
         """
         cases = (
             ("string", FlowResponse.Type.STRING, "string"),
@@ -218,7 +228,7 @@ class FlowResponseTests(TestCase):
             (
                 URL("https://example.org"),
                 FlowResponse.Type.URL,
-                URL("https://example.org"),
+                "https://example.org",
             ),
             ([1.1, 2.2, 3.3], FlowResponse.Type.ARRAY_OF_FLOAT, [1.1, 2.2, 3.3]),
             (
@@ -234,3 +244,367 @@ class FlowResponseTests(TestCase):
             self.assertEqual(response.response_type, flowtype)
             self.assertEqual(response.response_value, serialised)
             self.assertEqual(response.response, value)
+
+    def test_select_one_answer(self):
+        """
+        For answers to select_one must be a valid choice, and if choice_order is
+        present, it must be valid
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.SELECT_ONE
+        response.question.type_options["choices"] = ["a", "b", "c"]
+        response.response = "d"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response": [
+                    "d is not a valid choice. Valid choices are ['a', 'b', 'c']"
+                ]
+            },
+        )
+
+        response.response = "a"
+        response.full_clean()
+
+        response.response_metadata["choice_order"] = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response_metadata": ["choice_order must be an array"]},
+        )
+        response.response_metadata["choice_order"] = ["c", "d"]
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response_metadata": [
+                    "choice_order ['c', 'd'] contains choices not in ['a', 'b', 'c']"
+                ]
+            },
+        )
+
+        response.response_metadata["choice_order"] = ["c", "b", "a"]
+        response.full_clean()
+
+    def test_select_many(self):
+        """
+        answer must be an array of valid choices
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.SELECT_MANY
+        response.question.type_options["choices"] = ["a", "b", "c"]
+
+        response.response = "a"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(e.exception.message_dict, {"response": ["must be an array"]})
+
+        response.response = ["d"]
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response": ["['d'] contains choices not in ['a', 'b', 'c']"]},
+        )
+
+        response.response = ["b", "a"]
+        response.full_clean()
+
+        response.response_metadata["choice_order"] = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response_metadata": ["choice_order must be an array"]},
+        )
+
+    def test_numeric(self):
+        """
+        Numeric questions can be answered with an integer or float
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.NUMERIC
+
+        response.response = "a"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict, {"response": ["must be float or integer"]}
+        )
+
+        response.response = 1
+        response.full_clean()
+
+        response.response = 1.1
+        response.full_clean()
+
+    def test_text(self):
+        """
+        Text answers must be a string, with an optional language metadata
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.TEXT
+
+        response.response = 1
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(e.exception.message_dict, {"response": ["must be a string"]})
+
+        response.response = "answer"
+        response.full_clean()
+
+        response.response_metadata["language"] = 7
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response_metadata": ["language must be a string"]},
+        )
+
+        response.response_metadata["language"] = "eng"
+        response.full_clean()
+
+    def test_image(self):
+        """
+        Image answers are URLs, with optional format, dimensions, and file_size_mb
+        metadata
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.IMAGE
+
+        response.response = 1
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(e.exception.message_dict, {"response": ["must be a URL"]})
+
+        response.response = "https://example.org"
+        response.full_clean()
+
+        response.response_metadata["format"] = 7
+        response.response_metadata["dimensions"] = "invalid"
+        response.response_metadata["file_size_mb"] = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response_metadata": [
+                    "format must be a string",
+                    "dimensions must be an array",
+                    "file_size_mb must be integer or float",
+                ]
+            },
+        )
+
+        response.response_metadata["format"] = "image/png"
+        response.response_metadata["dimensions"] = ["a", "b", "c"]
+        response.response_metadata["file_size_mb"] = 3
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response_metadata": [
+                    "dimensions must have a length of 2",
+                    "dimensions items must be integers",
+                ]
+            },
+        )
+
+        response.response_metadata["dimensions"] = [640, 480]
+        response.response_metadata["file_size_mb"] = 3.7
+        response.full_clean()
+
+    def test_video(self):
+        """
+        Video answers are URLs, with optional format, language, dimensions,
+        file_size_mb, and duration_s metadata
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.VIDEO
+
+        response.response = "https://example.org"
+        response.response_metadata["format"] = "video/mp4"
+        response.response_metadata["dimensions"] = [1920, 1080]
+        response.response_metadata["file_size_mb"] = 243
+        response.response_metadata["language"] = 7
+        response.response_metadata["duration_s"] = "7"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response_metadata": [
+                    "duration_s must be integer or float",
+                    "language must be a string",
+                ]
+            },
+        )
+        response.response_metadata["language"] = "eng"
+        response.response_metadata["duration_s"] = 7
+        response.full_clean()
+
+    def test_audio(self):
+        """
+        Video answers are URLs, with optional format, language, file_size_mb, and
+        duration_s metadata
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.AUDIO
+
+        response.response = "https://example.org"
+        response.response_metadata["format"] = "audio/mp3"
+        response.response_metadata["language"] = "eng"
+        response.response_metadata["file_size_mb"] = 12.5
+        response.response_metadata["duration_s"] = 7.2
+        response.full_clean()
+
+    def test_geo_point(self):
+        """
+        geo_point answers are an array of floats
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.GEO_POINT
+
+        response.response = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response": ["must be an array"]},
+        )
+
+        response.response = ["a"]
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response": [
+                    "array may only contain floats",
+                    "number of array elements must be between 2 and 4 inclusive",
+                ]
+            },
+        )
+
+        response.response = [12.34, -12.34]
+        response.full_clean()
+
+    def test_datetime(self):
+        """
+        datetime answers are an RFC 3339 date-time string
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.DATETIME
+
+        response.response = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response": ["must be an RFC 3339 date-time"]},
+        )
+
+        response.response = "2021-02-03T04:05:06.123456+00:00"
+        response.full_clean()
+
+    def test_date(self):
+        """
+        date answers are an RFC 3339 date string
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.DATE
+
+        response.response = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response": ["must be an RFC 3339 date"]},
+        )
+
+        response.response = "2021-02-03"
+        response.full_clean()
+
+    def test_time(self):
+        """
+        time answers are an RFC 3339 time string
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.TIME
+
+        response.response = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response": ["must be an RFC 3339 time"]},
+        )
+
+        response.response = "04:05"
+        response.full_clean()
+
+    def test_open(self):
+        """
+        open question answers first get their type and type_options metadata validated,
+        then they're treated as whatever type is specified
+        """
+        response = self.create_response()
+        response.question.type = FlowQuestion.Type.OPEN
+
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {"response_metadata": ["type is required", "type_options is required"]},
+        )
+
+        response.response_metadata["type"] = "invalid"
+        response.response_metadata["type_options"] = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response_metadata": [
+                    "type must be one of ['audio', 'date', 'datetime', 'geo_point', "
+                    "'image', 'numeric', 'select_many', 'select_one', 'text', 'time', "
+                    "'video']",
+                    "type_options must be an object",
+                ]
+            },
+        )
+
+        response.response_metadata["type"] = FlowQuestion.Type.SELECT_ONE
+        response.response_metadata["type_options"] = {}
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response_metadata": [
+                    "type_options.choices is required for select_one type"
+                ]
+            },
+        )
+
+        response.response_metadata["type"] = FlowQuestion.Type.SELECT_ONE
+        response.response_metadata["type_options"] = {"choices": ["a", "b"]}
+        response.response = "invalid"
+        with self.assertRaises(ValidationError) as e:
+            response.full_clean()
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "response": [
+                    "invalid is not a valid choice. Valid choices are ['a', 'b']"
+                ]
+            },
+        )
+
+        response.response = "a"
+        response.full_clean()
