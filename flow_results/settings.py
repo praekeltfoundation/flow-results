b5cdf3dbb393d4441216c02b17818440f227d144
Hlamalani <hlamalani@praekelt.org>
2023-10-10 13:00:33 +0200

    Test against multiple postgres versions and change test db to postgres

--- flow_results/settings.py
+++ flow_results/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get(
             "FLOW_RESULTS_DATABASE",
-            "postgres://postgres@localhost:5432/flow_results",
+            "postgres://postgres@localhost/flow_results",
         ),
         engine="django.db.backends.postgresql",
     )
